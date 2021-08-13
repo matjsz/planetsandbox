f4cc8de661ae1ee5adeb34b7767b6b4e3438637b
matjs <matheusjgsilva3530@gmail.com>
2021-08-13 11:36:32 -0300

    cube.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-    res.send("index.html")
-})
+app.use("/js", express.static(__dirname + '/js'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log("running!")
